75e246165d4000d6c1ee3f5ee802fe58addfa720
Tomás Ferreira <tommatiasferreira@gmail.com>
2025-10-20 00:02:54 +0100

    Upload starter

---
+++ js/logos.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+        const logo = document.getElementById('logo');
+        if (!logo) return;
+
+        const idx = Math.floor(Math.random() * 26); // 0..25
+        const filename = `../img/logo/logo_${idx}.svg`;
+        // avoid setting same src (logo.src becomes absolute URL)
+        if (logo.getAttribute('src') === filename) return;
+        logo.setAttribute('src', filename);
+
+        logo.addEventListener('mouseleave', function(){
+            const idx = Math.floor(Math.random() * 26); // 0..25
+            const filename = `../img/logo/logo_${idx}.svg`;
+            // avoid setting same src (logo.src becomes absolute URL)
+            if (logo.getAttribute('src') === filename) return;
+            logo.setAttribute('src', filename);
+        });
+
+        logo.addEventListener('mouseenter', function(){
+            const idx = Math.floor(Math.random() * 26); // 0..25
+            const filename = `../img/logo/logo_${idx}.svg`;
+            // avoid setting same src (logo.src becomes absolute URL)
+            if (logo.getAttribute('src') === filename) return;
+            logo.setAttribute('src', filename);
+        });
+    });
