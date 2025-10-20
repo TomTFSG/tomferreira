83238d8adb2ea758b06d5955ddef04c3f5cc3010
Tomás Ferreira <120197183+TomTFSG@users.noreply.github.com>
2025-10-20 12:36:20 +0100

    Update logo file path to use relative URL

--- js/logos.js
+++ js/logos.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', function(){
         if (!logo) return;
 
         const idx = Math.floor(Math.random() * 26); // 0..25
-        const filename = `img/logo/logo_${idx}.svg`;
+        const filename = `../img/logo/logo_${idx}.svg`;
         // avoid setting same src (logo.src becomes absolute URL)
         if (logo.getAttribute('src') === filename) return;
         logo.setAttribute('src', filename);
 
         logo.addEventListener('mouseleave', function(){
             const idx = Math.floor(Math.random() * 26); // 0..25
-            const filename = `img/logo/logo_${idx}.svg`;
+            const filename = `../img/logo/logo_${idx}.svg`;
             // avoid setting same src (logo.src becomes absolute URL)
             if (logo.getAttribute('src') === filename) return;
             logo.setAttribute('src', filename);
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         logo.addEventListener('mouseenter', function(){
             const idx = Math.floor(Math.random() * 26); // 0..25
-            const filename = `img/logo/logo_${idx}.svg`;
+            const filename = `../img/logo/logo_${idx}.svg`;
             // avoid setting same src (logo.src becomes absolute URL)
             if (logo.getAttribute('src') === filename) return;
             logo.setAttribute('src', filename);
